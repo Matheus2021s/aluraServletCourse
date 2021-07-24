e101720f65c4a9e4af5206911d608d0237c039ba
Matheus Martins <matheusmartinsdacosta21@gmail.com>
2021-07-23 22:10:38 -0300

    Adding content type validation to define what kind of string will be retuned to the client: json or xml

--- src/br/com/mariah/gerenciador/servlet/EmpresasService.java
+++ src/br/com/mariah/gerenciador/servlet/EmpresasService.java
@@ -11,24 +11,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 import br.com.mariah.gerenciador.model.Banco;
 import br.com.mariah.gerenciador.model.Empresa;
-
 
 @WebServlet("/empresas")
 public class EmpresasService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			List<Empresa> empresas = new Banco().getEmpresas();
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		List<Empresa> empresas = new Banco().getEmpresas();
+
+		String accept = request.getHeader("Accept");
+
+		if (accept.contains("application/xml")) {
 			
-			 Gson gson = new Gson();
-			 String json = gson.toJson(empresas);
-			 
-			 response.setContentType("application/json");
-			 PrintWriter writer = response.getWriter();
-			 writer.print(json);
+			XStream xStream = new XStream();
+			xStream.alias("empresa", Empresa.class);
+			String xml = xStream.toXML(empresas);
+
+			response.setContentType("application/xml");
+			PrintWriter writer = response.getWriter();
+			writer.print(xml);
+		} else if (accept.contains("application/json")) {
+			
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+
+			response.setContentType("application/json");
+			PrintWriter writer = response.getWriter();
+			writer.print(json);
+		}
 	}
 
 }
