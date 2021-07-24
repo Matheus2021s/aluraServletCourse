c6e343a29b6480217ac04e3baea24c9a62441ecb
Matheus Martins <matheusmartinsdacosta21@gmail.com>
2021-07-23 22:43:04 -0300

    Adding init and destroy methods at filters that implements Filter interface. They became mandatory to deploy application on jetty or tomcat  in older versions

--- src/br/com/mariah/gerenciador/servlet/AutorizacaoFilter.java
+++ src/br/com/mariah/gerenciador/servlet/AutorizacaoFilter.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -15,15 +15,23 @@ import javax.servlet.http.HttpSession;
 //@WebFilter("/entrada")
 public class AutorizacaoFilter implements Filter {
 
-	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
-		
-		
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+			throws IOException, ServletException {
+
 		HttpServletRequest request = (HttpServletRequest) servletRequest;
-		HttpServletResponse response  = (HttpServletResponse) servletResponse;
-	
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+
 		HttpSession session = request.getSession();
 		String acao = request.getParameter("acao");
-		
+
 		Boolean isUsuarioNaoAutorizado = session.getAttribute("usuario") == null;
 		Boolean isAcaoProtegida = !(acao.equals("Autenticar") || acao.equals("AutenticarForm"));
 
@@ -31,9 +39,8 @@ public class AutorizacaoFilter implements Filter {
 			response.sendRedirect("entrada?acao=AutenticarForm");
 			return;
 		}
-		
+
 		chain.doFilter(request, response);
 	}
-
 
 }
